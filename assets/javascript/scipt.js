a22e70056ae95c799145bc901f2bfe9db0eff033
Brochb <brochbaltzer@gmail.com>
2023-10-25 09:59:20 -0600

    removed redudant comments no longer needed

--- assets/javascript/scipt.js
+++ assets/javascript/scipt.js
@@ -1,14 +1,9 @@
 var searchButton = document.getElementById("search-btn");
 
-// display history
-// Function to create the current weather display
 function createCurrentWeatherDisplay(data) {
     const displayWeatherDiv = document.getElementById("display-weather");
-    // Create an element to display the current weather
     const currentWeatherDiv = document.createElement("div");
     currentWeatherDiv.id = "current-weather";
-
-    // Populate the current weather display (modify as needed)
     currentWeatherDiv.innerHTML = `
         <h2>Current Weather</h2>
         <img src="${data.icon}" alt="Weather icon">
@@ -26,10 +21,8 @@ function createCurrentWeatherDisplay(data) {
     displayWeatherDiv.appendChild(currentWeatherDiv);
 }
 
-// Function to create a forecast card
 function createForecastCard(date, icon, temperature, windSpeed, humidity) {
-    const forecastCard = document.createElement("li"); // Create an <li> element
-    // Populate the forecast card (modify as needed)
+    const forecastCard = document.createElement("li");
     forecastCard.innerHTML = `
         <button>${date}</button>
         <img src="${icon}" alt="Weather icon">
@@ -40,32 +33,23 @@ function createForecastCard(date, icon, temperature, windSpeed, humidity) {
     return forecastCard;
 }
 
-// Event handler for the search button
 searchButton.addEventListener('click', function () {
-    // prep fetch variables
     const userInput = document.getElementById('search-box').value;
     const geocodingAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=1&appid=5fb9baa22b9c4016a10a5d9c4a88d1bd`;
 
-    // handle fetch calls
     fetch(geocodingAPI)
         .then(response => response.json())
         .then(data => {
             if (data.length > 0) {
-                // Extract the required values from the geocoding response
                 const lat = data[0].lat;
                 const lon = data[0].lon;
-
-                // input extracted data into weatherAPI
                 const weatherAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=5fb9baa22b9c4016a10a5d9c4a88d1bd`;
 
-                //fetch weather API
                 fetch(weatherAPI)
                     .then(response => response.json())
                     .then(weatherData => {
-                        // Define city name
                         const cityName = weatherData.city.name;
 
-                        // Create and populate the current weather display
                         const currentWeatherData = {
                             icon: weatherData.list[0].weather[0].icon,
                             temperature: weatherData.list[0].main.temp,
@@ -74,11 +58,9 @@ searchButton.addEventListener('click', function () {
                         };
                         createCurrentWeatherDisplay(currentWeatherData);
 
-                        // Create and populate forecast cards for the next 5 days
-                        const forecastData = weatherData.list.slice(1, 6); // Skip the first item (current weather)
+                        const forecastData = weatherData.list.slice(1, 6);
                         const forecastList = document.getElementById("display-forecast-list");
 
-                        // Clear existing forecast cards
                         forecastList.innerHTML = '';
 
                         forecastData.forEach(dayData => {
